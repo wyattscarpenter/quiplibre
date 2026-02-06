58f93e20201a8ef1d782a5c951b680f78f42dc1c
wyattscarpenter <wyattscarpenter@gmail.com>
2026-02-06 12:55:29 -0600

    adapt playwright basic test to my project

--- playwright.config.js
+++ playwright.config.js
@@ -72,10 +72,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
+  webServer: {
+    command: 'npx http-server -p 8080 .',
+    url: 'http://localhost:8080',
   //   reuseExistingServer: !process.env.CI,
-  // },
+  },
 });
 
--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,15 +1,16 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('has right title', async ({ page }) => {
+  await page.goto('http://localhost:8080/quiplibre.html');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Quiplibre/);
 });
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
+  //TODO: adapt this more
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
